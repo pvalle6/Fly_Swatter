4036f507cf2973d78107d7f1d5ea7f3a0cf67c3c
Peter Vallet <pvalle6@lsu.edu>
2023-12-17 17:32:50 -0600

    Create radar.py

---
+++ Fly_Swatter/radar.py
@@ -0,0 +1,14 @@
+# to calculate the ballistics of a target, two samples is the minimum required for a triangle to be created
+
+# simple example assumes a sonar image (probably want to generalize this to radar or image recognition)
+class target_loc():
+  def __init__(self, phi, theta, r, time_spot):
+    self.phi = phi
+    self.theta = theta
+    self.r = r
+    self.time_spot = time_spot
+
+def target_radar_sight(return_data):
+  # need a function that captures a location of the target
+  time = time.time()
+  return phi, theta, r, time
