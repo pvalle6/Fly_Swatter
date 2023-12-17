ac9d88984d2845d29cbb2ec0dec3a4e34b39a1e9
Peter Vallet <pvalle6@lsu.edu>
2023-12-17 17:40:44 -0600

    Create solution.py

---
+++ Fly_Swatter/solution.py
@@ -0,0 +1,7 @@
+# probably going to have to create a differential equation solver for this one
+deltaT, deltaXYZ, xyz_one, xyz_two = calculate_trajectory_target(first_loc, second_loc)
+
+guess_solution = xyz_two
+solution = xyz_two # need to replace this time factor with a better estimation
+
+solution = scipy.optimize.fsolve(find_firing_solution, guess_solution, args=(deltaXYZ, xyz_two, missile_speed))
