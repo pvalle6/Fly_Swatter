9a9ffa4f4b8be4d5de604b9be6a65968efe641e5
Peter Vallet <pvalle6@lsu.edu>
2023-12-18 01:54:22 -0600

    Create Mode_Control.py

---
+++ Fly_Swatter/Mode_Control.py
@@ -0,0 +1,17 @@
+from Fly_Swatter.Fly_Swatter import radar
+from Fly_Swatter.Fly_Swatter import search
+from Fly_Swatter.Fly_Swatter import fire_mode
+from Fly_Swatter.Fly_Swatter import target
+from Fly_Swatter.Fly_Swatter import graph_trajectory
+
+import numpy as np
+import scipy
+import time
+import sched
+import random
+
+search.search_mode()
+
+if target_mode == True:
+  track_lock()
+  
