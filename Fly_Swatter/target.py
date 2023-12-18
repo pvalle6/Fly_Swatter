a5e635b2ecffa5f4a946810681194da420519a2d
Peter Vallet <pvalle6@lsu.edu>
2023-12-18 02:08:29 -0600

    Update target.py

--- Fly_Swatter/target.py
+++ Fly_Swatter/target.py
@@ -7,7 +7,8 @@ import random
 from Fly_Swatter.Fly_Swatter import radar
 
 def calculate_trajectory_target(first_data, second_data):
-  # probably want to convert to cartesian coordinates immedatiately unless there is a spherical coordinate formula
+  # this data takes two recording of target data, calculates the derivatives
+  # and converts the data from spherical to cartesian coordinates for easy of calculation 
 
   x_one = (first_data.r) * (np.cos(first_data.theta)) * (np.sin(first_data.phi))
   y_one = (first_data.r) * (np.sin(first_data.theta)) * (np.sin(first_data.phi))
@@ -31,14 +32,15 @@ def calculate_trajectory_target(first_data, second_data):
   return [deltaT, deltaXYZ, xyz_one, xyz_two]
 
 def recalculate_spherical(deltaX, deltaY, deltaZ, deltaT, x, y, z):
-
+  # this function converts derivatives from cartestian to spherical coordinates
   dtheta_dt  = (-x / ((x ** 2) + (y ** 2)) ) * (deltaX) + (x / ((x ** 2) + (y ** 2)) ) * (deltaY)
   dphi_dt = ((z * x_two) / (sqrt(x ** 2 + y **2) * (x ** 2 + z **2 + y **2))) * (deltaX)  + ((z * x) / (sqrt(x ** 2 + y **2) * (x ** 2 + z **2 + y **2))) * (deltaY) - (sqrt(y ** 2 + x ** 2) / (z ** 2 + x **2 + y **2 )) * deltaZ
   dp_dt = (x / sqrt (x ** 2 + y ** 2 + z ** 2)) * deltaX + (y / sqrt(x ** 2 + y ** 2  + z **2)) * deltaY + (z / sqrt(x ** 2 + y ** 2  + z **2)) * deltaZ 
   return [dtheta_dt, dphi_dt, dp_dt]
 
 def calculate_ballistics_missile(speed, phi, theta):
-  # probably want to convert to cartesian coordinates immedatiately unless there is a spherical coordinate formula
+  # this calculates the cartestian vector of the missile from launch
+  # makes solving differential equation easier
 
   deltaX = (speed) * (np.cos(theta)) * (np.sin(phi))
   deltaY = (speed) * (np.sin(theta)) * (np.sin(phi))
@@ -48,6 +50,7 @@ def calculate_ballistics_missile(speed, phi, theta):
 
 def find_firing_solution(x, deltaXYZ, xyz_two, missile_speed):
   # x0 is time, x1 is phi, x2 is theta, 
+  # this is the function used by scipy to solve as a set of three equations 
   m_x_speed, m_y_speed, m_z_speed = calculate_ballistics_missile(missile_speed, x[1], x[2])
   t_x_speed, t_y_speed, t_z_speed = deltaXYZ
   t_x_pos, t_y_pos, t_z_pos = xyz_two
