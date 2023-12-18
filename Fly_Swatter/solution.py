d56d2026555d0add876118933e341850c807141a
Peter Vallet <pvalle6@lsu.edu>
2023-12-18 02:48:14 -0600

    Update solution.py

--- Fly_Swatter/solution.py
+++ Fly_Swatter/solution.py
@@ -9,6 +9,6 @@ from Fly_Swatter.Fly_Swatter import target
 deltaT, deltaXYZ, xyz_one, xyz_two = calculate_trajectory_target(first_loc, second_loc)
 
 guess_solution = xyz_two
-solution = xyz_two # need to replace this time factor with a better estimation
+guess_solution = xyz_two # need to replace this time factor with a better estimation
 
 solution = scipy.optimize.fsolve(find_firing_solution, guess_solution, args=(deltaXYZ, xyz_two, missile_speed))
