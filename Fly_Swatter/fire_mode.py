284650a14b0e0a732562119c460fdadc1ec50d8b
Peter Vallet <pvalle6@lsu.edu>
2023-12-17 23:26:57 -0600

    Create fire_mode.py

---
+++ Fly_Swatter/fire_mode.py
@@ -0,0 +1,16 @@
+from Fly_Swatter.Fly_Swatter import radar
+from Fly_Swatter.Fly_Swatter import target
+
+def target_mode():
+  # should activate when first detect a target from search mode 
+
+  # should schedule a check to see if a firing solution is avaliable
+  # if none is avalaible, two options, calculate a likely probably solution or
+  # wait and check for another solution if avaliable
+
+
+  # might want to create a return to search mode 
+
+  # need to slave RADAR and launcher together when target mode is activated
+
+  # once firing solution is calculated, fire, and reset
