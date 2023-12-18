8b78c77047b65d483af64ebe0ac7eda713b3325c
Peter Vallet <pvalle6@lsu.edu>
2023-12-17 19:36:47 -0600

    Update radar.py
    added generate_random_vector function

--- Fly_Swatter/radar.py
+++ Fly_Swatter/radar.py
@@ -14,3 +14,13 @@ def target_radar_sight(return_data):
   # need a function that captures a location of the target
   time = time.time()
   return phi, theta, r, time
+
+def generate_random_vector(max_distance):
+  a = random.random() * (np.pi / 2)
+  b = random.random() * (2 * np.pi)
+
+  data_one = target_loc(a, b, random.random() * max_distance, 0)
+
+  data_two = target_loc(a + 0.01, b + 0.01, 2 + 0.01, 0.2)
+
+  return [data_one, data_two]
