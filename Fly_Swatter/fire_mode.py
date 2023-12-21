d0f6c0140c9f8a1202eca011fea1ac2905875740
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 05:21:49 -0600

    Update fire_mode.py
    removed azimuth control and moved it to mechanics.py

--- Fly_Swatter/fire_mode.py
+++ Fly_Swatter/fire_mode.py
@@ -59,42 +59,4 @@ def track_lock(realism = 0, projectile_type = "bullet", target_course = "straigh
       log = ("FIRING SOLUTION RESULTS: \n") + (f"REAL TIME: {time.time()} \n" + (f"Time to Target: {solution[0]}\nPhi to Target: {solution[1]}\nTheta to Target: {solution[2]}"))
   return log
   
-  
-  # should activate when first detect a target from search mode 
 
-  # should schedule a check to see if a firing solution is avaliable
-  # if none is avalaible, two options, calculate a likely probably solution or
-  # wait and check for another solution if avaliable
-
-
-  # might want to create a return to search mode 
-
-  # need to slave RADAR and launcher together when target mode is activated
-
-  # once firing solution is calculated, fire, and reset
-
-# class azimuth_control():
-#   # class for controlling the orientation of the launch system and radar
-#   # need to implement controls for slaving launch system to radar
-#   def __init__(self, name, status, theta, phi, x, y, z):
-#     self.name = name
-#     self.status = status
-#     self.theta = theta
-#     self.phi = phi
-#     self.x = x
-#     self.y = y
-#     self.z = z 
-
-#   def set_position(theta, phi, x, y, z):
-#     self.theta = theta
-#     self.phi = phi
-#     self.x = x
-#     self.y = y
-#     self.z = z
-
-#   def change_azimuth(d_theta, d_phi):
-#     self.theta = self.theta + d_theta
-#     self.phi = self.phi + d_phi
-
-#   def slave(controller):
-#     self.status = controller
