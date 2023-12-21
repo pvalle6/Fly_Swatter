eec6f5d4eeacf356750b64dfcb23e1f308d02a4f
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 00:40:33 -0600

    Update test_fire.py
    added a testing function for the track_lock

--- tests/test_fire.py
+++ tests/test_fire.py
@@ -15,6 +15,7 @@ from Fly_Swatter.Fly_Swatter import graph_trajectory
 class Test_Fire(unittest.TestCase):
   def test_validity(self):
     self.assertEqual(fire_mode.check_valdity([-1, 0.2, 0.3]), False)
-
+  def test_basic_fire(self):
+    self.assertEqual(fire_mode.track_lock(seed = 1)[:23], "FIRING SOLUTION RESULTS")
 if __name__ == '__main__':
   unittest.main(argv=['first-arg-is-ignored'], exit=False)
