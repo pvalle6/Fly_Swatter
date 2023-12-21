4af245d0bce0599bc5378f6511fcc5a3ce0851a8
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 08:25:32 -0600

    Update graph_trajectory.py
    added ballistic trajectory function

--- Fly_Swatter/graph_trajectory.py
+++ Fly_Swatter/graph_trajectory.py
@@ -40,3 +40,38 @@ def graph_solution(missile_speed, phi, theta, deltaXYZ_target, xyz_target, time)
   
   ax.legend()
   plt.show()
+
+def plot_ballistic_trajectory(initial_missile_speed, phi, theta, deltaXYZ_target, xyz_target, time):
+  # function that enables a quick 3d plot of the missile launch and interception of a straight-course target
+  # probably need to update with a non-straight course target and non straight missile path
+  mx, my, mz = target.calculate_ballistics_missile(initial_missile_speed, phi, theta)
+
+  tx, ty, tz = xyz_target
+  dtx, dty, dtz = deltaXYZ_target
+
+  fig = plt.figure()
+  ax = plt.axes(projection='3d')
+
+
+  txline = np.linspace(tx,tx + dtx * time, 1000)
+  tyline = np.linspace(ty,ty + dty * time, 1000)
+  tzline = np.linspace(tz,tz + dtz * time, 1000)
+
+  g = 9.81
+
+  mxline = np.linspace(0,mx * time,100)
+  myline = np.linspace(0,my * time ,100)
+  z_parabola = -(g / 2) * ((myline / my) ** 2) + (mz * (myline / my))
+
+
+  ax.legend()
+
+  ax.set_xlabel('x')
+  ax.set_ylabel('y')
+  ax.set_zlabel('z');
+
+  ax.plot3D(txline, tyline, tzline, 'grey', label='Target Trajectory')
+  ax.plot3D(mxline, myline, z_parabola, 'red', label='Missile Trajectory')
+
+  ax.legend()
+  plt.show()
