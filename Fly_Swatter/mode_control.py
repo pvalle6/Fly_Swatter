31d1094551e58fab29dd5cbc0d9c9805a445a1d8
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 05:36:35 -0600

    Update mode_control.py
    added graphical and verbose control based on updated log return

--- Fly_Swatter/mode_control.py
+++ Fly_Swatter/mode_control.py
@@ -11,9 +11,15 @@ import sched
 import random
 
 # this should be the master script of the comptuer that controls the interface between the two modes 
-def system_run(search_runs = 1, seed_search = 0, seed_fire = 0, verbose = False, graphical = False):
+def system_run(search_runs = 1, seed_search = 0, seed_fire = 0, verbose = False, graphical = False, realism = 0):
   target_mode = search.search_mode(runs = search_runs, seed = seed_search)
   if target_mode:
     log = fire_mode.track_lock(seed = seed_fire, graphical = graphical)
+    solution, deltaXYZ, xyzTwo, missile_speed = log
     if verbose:
-      print(log)
+      print_log(solution)
+    if graphical:
+      if realism == 0:
+        graph_solution(missile_speed, solution[1], solution[2], deltaXYZ, xyzTwo, solution[0]):
+def print_log(solution):
+  print(("FIRING SOLUTION RESULTS: \n") + (f"REAL TIME: {time.time()} \n" + (f"Time to Target: {solution[0]}\nPhi to Target: {solution[1]}\nTheta to Target: {solution[2]}"))
