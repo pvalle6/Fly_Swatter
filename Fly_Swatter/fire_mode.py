40cacda9c595a56e5ba802d042a32b2a09be00ff
Peter Vallet <pvalle6@lsu.edu>
2023-12-20 23:43:07 -0600

    Update fire_mode.py
    track_lock now returns the log which can be optionally printed and there is now a graphical arg

--- Fly_Swatter/fire_mode.py
+++ Fly_Swatter/fire_mode.py
@@ -7,43 +7,48 @@ import random
 from Fly_Swatter.Fly_Swatter import radar
 from Fly_Swatter.Fly_Swatter import target
 
-# might want to have this part coded before the switch to improve computational effiency and not delay time to solution
 def check_valdity(solution):
   if solution[0] <= 0.05:
     return False
   else:
     return True
-    
-def laser_handler(first_loc):
-  missile_speed = 10
+
+def laser_handler(first_loc, missile_speed = 10):
+
   deltaT, deltaXYZ, xyz_one, xyz_two = radar.calculate_trajectory_target(first_loc)
   guess_solution = xyz_two
   solution = scipy.optimize.fsolve(target.laser_solution, guess_solution, args=(deltaXYZ, xyz_two, missile_speed))
   if(check_valdity(solution)):
-    return [solution, True]
+    return [solution, True, deltaXYZ, xyz_two, missile_speed]
   else:
-    return [None, False]
+    return [None, False, None, None]
   
-def track_lock(realism = 0, projectile_type = "bullet", target_course = "straight", seed = None):
+def track_lock(realism = 0, projectile_type = "bullet", target_course = "straight", seed = None, graphical = False):
   # three realism levels to calculate for 
   if realism == 0 and projectile_type == "bullet" and target_course == "straight":
     # this case is basically a single fire laser
     validity = False
     #only part actually coded for
-    print("SOLUTION INCOMING")
+    print("SOLUTION INCOMING \n")
     first_loc = radar.generate_random_vector(2, 2) # this needs to substituted for the same sim as the search mode
 
-    solution, validity = laser_handler(first_loc)
+    solution, validity, deltaXYZ, xyzTwo, missile_speed = laser_handler(first_loc)
     if validity:
-      print("Solution Found")
-      print(f"Time to Target: {solution[0]}")
-      print(f"Phi to Target: {solution[1]}")
-      print(f"Theta to Target: {solution[2]}")
+      # print("Solution Found")
+      # print(f"Time to Target: {solution[0]}")
+      # print(f"Phi to Target: {solution[1]}")
+      # print(f"Theta to Target: {solution[2]}")
+
+      if graphical:
+          graph_trajectory.graph_solution(missile_speed, solution[1], solution[2], deltaXYZ_target, xyz_target, solution[0])
+      log = ("FIRING SOLUTION RESULTS: \n") + (f"REAL TIME: {time.time()} \n" + (f"Time to Target: {solution[0]}\nPhi to Target: {solution[1]}\nTheta to Target: {solution[2]}"))
     else:
-      print("NO SOLUTION YET AVALIABLE, INVALID AZMIMUTH")
+      # print("NO SOLUTION YET AVALIABLE, INVALID AZMIMUTH")
+      log = ("FIRING SOLUTION RESULTS: \n") + (f"REAL TIME: {time.time()}" + "NO SOLUTION YET AVALIABLE, INVALID AZMIMUTH")
   else:
-      print("NO SOLUTION YET AVALIABLE, OUT OF BOUNDS")
-    
+      # print("NO SOLUTION YET AVALIABLE, OUT OF BOUNDS")
+      log = ("FIRING SOLUTION RESULTS: \n")+ (f"REAL TIME: {time.time()}" + "NO SOLUTION YET AVALIABLE, OUT OF BOUNDS")
+  return log
   
   # should activate when first detect a target from search mode 
 
