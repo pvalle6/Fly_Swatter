0323143f8ed981b433afbd7b184cc02f361252a5
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 05:40:13 -0600

    Update mode_control.py
    testing caught parentheses error

--- Fly_Swatter/mode_control.py
+++ Fly_Swatter/mode_control.py
@@ -22,4 +22,4 @@ def system_run(search_runs = 1, seed_search = 0, seed_fire = 0, verbose = False,
       if realism == 0:
         graph_solution(missile_speed, solution[1], solution[2], deltaXYZ, xyzTwo, solution[0])
 def print_log(solution):
-  print(("FIRING SOLUTION RESULTS: \n") + (f"REAL TIME: {time.time()} \n" + (f"Time to Target: {solution[0]}\nPhi to Target: {solution[1]}\nTheta to Target: {solution[2]}"))
+  print( "FIRING SOLUTION RESULTS: \n" + f"REAL TIME: {time.time()} \n" + f"Time to Target: {solution[0]}\nPhi to Target: {solution[1]}\nTheta to Target: {solution[2]}")
