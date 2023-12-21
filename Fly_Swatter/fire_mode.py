7ee10fd04646d059599c616b9f90b4d20b142ffa
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 00:27:02 -0600

    Update fire_mode.py
    commented out azimuth control as it is not used yet

--- Fly_Swatter/fire_mode.py
+++ Fly_Swatter/fire_mode.py
@@ -64,28 +64,28 @@ def track_lock(realism = 0, projectile_type = "bullet", target_course = "straigh
 
   # once firing solution is calculated, fire, and reset
 
-class azimuth_control():
-  # class for controlling the orientation of the launch system and radar
-  # need to implement controls for slaving launch system to radar
-  def __init__(self, name, status, theta, phi, x, y, z):
-    self.name = name
-    self.status = status
-    self.theta = theta
-    self.phi = phi
-    self.x = x
-    self.y = y
-    self.z = z 
+# class azimuth_control():
+#   # class for controlling the orientation of the launch system and radar
+#   # need to implement controls for slaving launch system to radar
+#   def __init__(self, name, status, theta, phi, x, y, z):
+#     self.name = name
+#     self.status = status
+#     self.theta = theta
+#     self.phi = phi
+#     self.x = x
+#     self.y = y
+#     self.z = z 
 
-  def set_position(theta, phi, x, y, z):
-    self.theta = theta
-    self.phi = phi
-    self.x = x
-    self.y = y
-    self.z = z
+#   def set_position(theta, phi, x, y, z):
+#     self.theta = theta
+#     self.phi = phi
+#     self.x = x
+#     self.y = y
+#     self.z = z
 
-  def change_azimuth(d_theta, d_phi):
-    self.theta = self.theta + d_theta
-    self.phi = self.phi + d_phi
+#   def change_azimuth(d_theta, d_phi):
+#     self.theta = self.theta + d_theta
+#     self.phi = self.phi + d_phi
 
-  def slave(controller):
-    self.status = controller
+#   def slave(controller):
+#     self.status = controller
