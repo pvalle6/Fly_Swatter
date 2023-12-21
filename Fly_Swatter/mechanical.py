2590eea123742a2cceea8f4a1eb186c8c763a640
Peter Vallet <pvalle6@lsu.edu>
2023-12-21 05:20:53 -0600

    Create mechanical.py

---
+++ Fly_Swatter/mechanical.py
@@ -0,0 +1,31 @@
+  # might want to create a return to search mode 
+
+  # need to slave RADAR and launcher together when target mode is activated
+
+  # once firing solution is calculated, fire, and reset
+
+# class azimuth_control():
+#   # class for controlling the orientation of the launch system and radar
+#   # need to implement controls for slaving launch system to radar
+#   def __init__(self, name, status, theta, phi, x, y, z):
+#     self.name = name
+#     self.status = status
+#     self.theta = theta
+#     self.phi = phi
+#     self.x = x
+#     self.y = y
+#     self.z = z 
+
+#   def set_position(theta, phi, x, y, z):
+#     self.theta = theta
+#     self.phi = phi
+#     self.x = x
+#     self.y = y
+#     self.z = z
+
+#   def change_azimuth(d_theta, d_phi):
+#     self.theta = self.theta + d_theta
+#     self.phi = self.phi + d_phi
+
+#   def slave(controller):
+#     self.status = controller
