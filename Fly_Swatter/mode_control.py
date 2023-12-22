a93e9047ead1ed06438032a9efdf9b8e517cf8ad
Peter Vallet <pvalle6@lsu.edu>
2023-12-22 06:19:24 -0600

    Update mode_control.py
    provided parameter for first loc from search

--- Fly_Swatter/mode_control.py
+++ Fly_Swatter/mode_control.py
@@ -20,9 +20,9 @@ def system_run(search_runs = 1, seed_search = None, seed_fire = None, verbose = 
       graphical: default = false, provides for if a graph should be printed or not
       realism: default = 0, 0 provides for laser projectile, gravityless, no air resistance; 1 provides for gravity based projectile
   """
-  target_mode = search.search_mode(runs = search_runs, seed = seed_search)
-  if target_mode:
-    log = fire_mode.track_lock(seed = seed_fire, realism = realism)
+  fire_on, first_loc = search.search_mode(runs = search_runs, seed = seed_search)
+  if fire_on:
+    log = fire_mode.track_lock(seed = seed_fire, realism = realism, first_loc = first_loc)
     solution, deltaXYZ, xyzTwo, missile_speed = log
     if verbose:
       print_log(solution)
