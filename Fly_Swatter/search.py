c03ca23acaa39d9a0b5870590515dd7ee085bbf7
Peter Vallet <pvalle6@lsu.edu>
2023-12-23 05:25:34 -0600

    Update search.py
    added type hinting

--- Fly_Swatter/search.py
+++ Fly_Swatter/search.py
@@ -10,7 +10,7 @@ from Fly_Swatter.Fly_Swatter import fire_mode
 from Fly_Swatter.Fly_Swatter import target
 from Fly_Swatter.Fly_Swatter import graph_trajectory
 
-def target_check(image = None, search_seed = None, fire_seed = None):
+def target_check(image = None, search_seed: int = None, fire_seed: int = None):
   """ Provides a simulation of detecting a airbourne target
       image: default = none, this is just a placeholder for image/radar detection
       seed: default = none, this is provided through mode_control, and used in probability calculation; 10 returns True always
@@ -25,7 +25,7 @@ def target_check(image = None, search_seed = None, fire_seed = None):
   else:
     return [False, None]
   
-def search_mode(runs = 10, seed_fire = None, seed_search = None):
+def search_mode(runs: int = 10, seed_fire: int = None, seed_search: int = None):
   """ provides for a scheduling of a scanning event to simulate a radar search
       runs: default = 10, this provides how many runs and probabilities should be calculated until something is found or retiring
       seed: default = none, this is provided through mode_control and is used in target_check, and used in probability calculation; 10 returns True always
