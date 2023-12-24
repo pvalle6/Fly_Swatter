28d950487bb2228f00e255864da2665e35e007e9
Peter Vallet <pvalle6@lsu.edu>
2023-12-24 12:09:37 -0600

    Update mode_control.py
    changed naming convention in database

--- Fly_Swatter/mode_control.py
+++ Fly_Swatter/mode_control.py
@@ -28,7 +28,7 @@ def system_run(search_runs: int = 1, seed_search: int = None, seed_fire: int = N
   # this handles the plotting on the radar display image
   for i in target_list:
     # going to want to move this into the search mode module
-    contact_list.append(radar.contact(str(id), last_time = time.time(), last_loc = i, status = "unknown"))
+    contact_list.append(radar.contact("Contact " + str(id), last_time = time.time(), last_loc = i, status = "unknown"))
     id = id + 1
   main_db = radar.contact_database(name = "main", contacts = contact_list)
   if graphical and fire_on:
