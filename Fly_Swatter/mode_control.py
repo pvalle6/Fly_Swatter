4737e0903f99b586ddc19326389985d27668c00a
pvalle6 <pvalle6@lsu.edu>
2023-12-31 17:22:37 +0000

    refactors args from system_run into its own class

--- Fly_Swatter/mode_control.py
+++ Fly_Swatter/mode_control.py
@@ -10,8 +10,19 @@ import time
 import sched
 import random
 
+class system_run_args():
+  def __init__(self, search_runs: int = 1, seed_search: int = None, seed_fire: int = None, verbose: bool = False, graphical: bool = False, realism: int = 0, engage: bool = True):
+    self.search_runs = search_runs
+    self.seed_search = seed_search
+    self.seed_fire = seed_fire
+    self.verbose = verbose
+    self.graphical = graphical
+    self.realism = realism
+    self.engage = engage
+    
+
 # this should be the master script of the comptuer that controls the interface between the two modes 
-def system_run(search_runs: int = 1, seed_search: int = None, seed_fire: int = None, verbose: bool = False, graphical: bool = False, realism: int = 0, engage: bool = True):
+def system_run(args):
   """ This is the main function for running the program. It starts both search_mode and if applicable fire_mode
       search_runs: default = 1, indicates how many times search_mode can run for if nothing is found
       seed_search: default = None, provides a seed for the calculation of the probablility of search finding something in a simulation; 10 returns True Everytime
@@ -23,25 +34,25 @@ def system_run(search_runs: int = 1, seed_search: int = None, seed_fire: int = N
   p_list = []
   contact_list = []
   id = 1
-  fire_on, target_list = search.search_mode(runs = search_runs, seed_fire = seed_fire, seed_search = seed_search)
+  fire_on, target_list = search.search_mode(runs = args.search_runs, seed_fire = args.seed_fire, seed_search = args.seed_search)
   for i in target_list:
     # going to want to move this into the search mode module
     contact_list.append(radar.contact(str(id), last_time = time.time(), last_loc = i, status = "unknown"))
     id = id + 1
   main_db = radar.contact_database(name = "main", contacts = contact_list)
-  if graphical and fire_on:
+  if args.graphical and fire_on:
     for i in target_list:
       x, y, z = target.calculate_ballistics_missile(i.r, i.phi, i.theta)
       p_list.append([x,y,z])
     graph_trajectory.plot_radar(p_list)
-  if engage and fire_on:
-    log = fire_mode.track_lock(seed = seed_fire, realism = realism, first_loc = target_list[0])
+  if args.engage and fire_on:
+    log = fire_mode.track_lock(seed = args.seed_fire, realism = args.realism, first_loc = target_list[0])
     solution, deltaXYZ, xyzTwo, missile_speed = log
     if check_null(solution, [None, None, None]) == False:
-      if verbose:
+      if args.verbose:
         print_log(solution)
-      if graphical:
-        if realism == 0:
+      if args.graphical:
+        if args.realism == 0:
           graph_trajectory.graph_solution(missile_speed, solution[1], solution[2], deltaXYZ, xyzTwo, solution[0])
         if realism == 1:
           graph_trajectory.plot_ballistic_trajectory(missile_speed, solution[1], solution[2], deltaXYZ, xyzTwo, solution[0])
